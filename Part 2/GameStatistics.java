cf734c4b0ca94f98d38edeb7347f6f5c0d597d1a
Ashar Adeel <asharadeel@outlook.com>
2025-04-26 01:01:19 +0100

    Fixed game stats for end of game status.
    all horses work now and all data is shown at the end of a game

--- Part 2/GameStatistics.java
+++ Part 2/GameStatistics.java
@@ -6,27 +6,9 @@ public class GameStatistics {
     private boolean raceFinished;
 
     //declaration
-    GameStatistics(int distance, Horse[] horses) {
+    GameStatistics(int distance) {
         this.raceLength = distance;
-        for(Horse x: horses){
-            if(x != null) {
-                HorseData current = new HorseData(x);
-                RaceHorseData = AALibrary.appendValue(RaceHorseData, current);
-            }
-            else{
-                RaceHorseData = AALibrary.appendValue(RaceHorseData, null);
-            }
-        }
-        raceFinished = false;
-
-        System.out.println("- GAME STATS STARTED FOR RACE  -----------------------");
-        System.out.println("Race Length: " + raceLength);
-        for(HorseData x : RaceHorseData){
-            if(x != null) {
-                System.out.println(x.getSymbol() + " | Name: " + x.getName() + " | Conf: " + x.getConfidence());
-            }
-        }
-        System.out.println("------------------------------------------------------");
+       raceFinished = false;
     }
 
     public HorseData[] getHorses(){
@@ -34,6 +16,7 @@ public class GameStatistics {
     }
 
     public void finished(){
+        setFinished();
         if(raceFinished){
             AALibrary.printHorses(RaceHorseData);
         }
@@ -41,79 +24,49 @@ public class GameStatistics {
     //accessors and mutators
     public void setWinner(Horse h) {
         winner = h;
+        HorseData w = new HorseData(h);
     }
 
     public void setFinished(){
         raceFinished = true;
     }
 
-    /*
-    Active detection during game to track statistcs.
-     */
-    public void detect(){
-        detectFall();
-        trackTime();
 
-        System.out.println("detect()");
-    }
-
-
-
-    private void detectFall(){
-        for(int i = 0; i < 10; i++){
-            try {
-                if (RaceHorseData[i] != null) {
-                    if (RaceHorseData[i].hasFallen() && !RaceHorseData[i].isHorseChecked()) {
-                        HorseData current = RaceHorseData[i];
-                        double avg = calculateAvgSpeed(current, time);
-                        current.setTimeTaken(time);
-                        current.setAverageSpeed(avg);
-                        current.setHorseChecked(true);
-                    }
-                }
-            }
-            catch(Exception e){
-                System.out.println(e);
+    public void HorseFinished(Horse h, int index) {
+        // First check if this horse already exists in RaceHorseData
+        boolean horseExists = false;
+        for (HorseData b : RaceHorseData) {
+            if (b != null && b.getSymbol() == h.getSymbol()) {
+                horseExists = true;
+                break;
             }
         }
+
+        // Only create new HorseData if it doesn't exist
+        if (!horseExists) {
+            HorseData x = new HorseData(h);
+            x.setDistanceTravelled(h.getDistanceTravelled());
+            double avg = calculateAvgSpeed(x, time);
+            x.setHorseChecked(true);
+            x.setTimeTaken(time);
+            x.setAverageSpeed(avg);
+            x.setLane(index+1);
+            System.out.println("avg " + avg);
+            System.out.println("time " + time);
+            System.out.println("index " + index);
+            RaceHorseData = AALibrary.appendValue(RaceHorseData, x);
+            System.out.println("HorseData made for " + x.getName());
+        }
     }
+
 
     //maths
     public void trackTime(){
         time++;
     }
 
-
-    public static double FindWeatherFactor(){
-        int speedpercentage = 100;
-        for(int i = 0; i< 20; i++){
-            //System.out.println(Math.random());
-            long val =Math.round(Math.random()*100)%5;
-            if(val < 1){
-                speedpercentage--;
-                continue;
-            }
-            else if(val<2){
-                speedpercentage-= 3;
-                continue;
-            }
-            else if(val <3){
-                speedpercentage-= 6;
-            }
-        }
-        return speedpercentage/100.0;
-    }
-
-
-
-
     public static double calculateAvgSpeed(Horse h, int t){
         double x = (double) h.getDistanceTravelled();
         return (Math.round((x/t*10.0)*10.0)/2.0);
     }
-
-
-
-
-
 }
